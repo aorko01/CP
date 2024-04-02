b4d1273bbe441bb97756675045fa60e87f8cb22e
Shahir BIn Zulfiker <2105149@ugrad.cse.buet.ac.bd>
2024-04-03 05:48:50 +0600

    actually forth one

--- Brute/B_Pleasant_Pairs.cpp
+++ Brute/B_Pleasant_Pairs.cpp
@@ -1,3 +1,7 @@
+/*
+    REFER TO THIS LINK FOR EXPLAINATION
+        https://www.notion.so/e05f3660266343c4b140814d0467d36b?pvs=4
+*/
 #include <bits/stdc++.h>
 using namespace std;
 typedef long long ll;
