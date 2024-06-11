47b929b12137e48f9b03c6d1928a70246937d81c
Shahir BIn Zulfiker <2105149@ugrad.cse.buet.ac.bd>
2024-06-11 07:55:59 +0600

    cool

---
+++ Implementation_goodies/B_Unnatural_Conditions.cpp
@@ -0,0 +1,29 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef pair<int,int> pi;
+typedef pair<ll,ll> pl;
+#define F first
+#define S second
+#define PB push_back
+#define MP make_pair
+#define REP(i,a,b) for(int i=a;i<b;i++)
+#define SQ(a) (a)*(a)
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    int n,m;
+    cin>>n>>m;
+    REP(i,0,200)
+    cout<<9;
+    REP(i,0,199)
+    cout<<0;
+    cout<<1<<endl;
+    REP(i,0,200)
+    cout<<9;
+
+}
