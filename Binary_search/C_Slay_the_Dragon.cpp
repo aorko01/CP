d8aae8f95938bf5b7f85c718dc089c840731718d
Shahir BIn Zulfiker <2105149@ugrad.cse.buet.ac.bd>
2024-07-27 10:01:22 +0600

    good

--- Binary_search/C_Slay_the_Dragon.cpp
+++ Binary_search/C_Slay_the_Dragon.cpp
@@ -1,54 +1,33 @@
-#include <bits/stdc++.h>
-using namespace std;
-typedef long long ll;
-typedef vector<ll> vl;
-typedef pair<ll, ll> pl;
-#define F first
-#define S second
-#define PB push_back
-#define MP make_pair
-#define REP(i, a, b) for (int i = a; i < b; i++)
-#define SQ(a) (a) * (a)
+#include <iostream>
+#include <vector>
+#include <string>
 
-int main() {
-    ios::sync_with_stdio(false);
-    cin.tie(0);
-    
-    ll n;
-    cin >> n;
-    vl v(n);
-    ll total = 0;
-    REP(i, 0, n) {
-        cin >> v[i];
-        total += v[i];
+std::vector<int> lengthOfZeroBlocks(const std::string &binaryString) {
+    std::vector<int> zeroLengths;
+    int currentLength = 0;
+    bool inBlock = false;
+
+    for (char ch : binaryString) {
+        if (ch == '1') {
+            if (inBlock && currentLength > 0) {
+                zeroLengths.push_back(currentLength);
+            }
+            inBlock = true;
+            currentLength = 0;
+        } else if (ch == '0' && inBlock) {
+            currentLength++;
+        }
     }
 
-    sort(v.begin(), v.end());
+    return zeroLengths;
+}
 
-    int t;
-    cin >> t;
-    while (t--) {
-        ll x, y;
-        cin >> x >> y;
-        ll coins = LLONG_MAX;
+int main() {
+    std::string binaryString = "10101";
+    std::vector<int> zeroLengths = lengthOfZeroBlocks(binaryString);
 
-        auto it = lower_bound(v.begin(), v.end(), x);
-
-        if (it == v.end()) {
-            it--;
-            //this is a new way of taking only the numbers that results to positive after subtraction.
-            coins = max(0LL, x - *it) + max(0LL, y - (total - *it));
-        } else {
-            coins = max(0LL, x - *it) + max(0LL, y - (total - *it));
-
-
-            if (it != v.begin()) {
-                it--;
-                coins = min(coins, max(0LL, x - *it) + max(0LL, y - (total - *it)));
-            }
-        }
-
-        cout << coins << endl;
+    for (int length : zeroLengths) {
+        std::cout << length << " ";
     }
 
     return 0;
