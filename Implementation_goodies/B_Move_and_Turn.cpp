50dd2b965f92441cea4ded8ea12ee98e3074e6af
Shahir BIn Zulfiker <2105149@ugrad.cse.buet.ac.bd>
2024-09-09 15:59:39 +0600

    a goodie

---
+++ Implementation_goodies/B_Move_and_Turn.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef pair<int, int> pi;
+typedef pair<ll, ll> pl;
+#define F first
+#define S second
+#define PB push_back
+#define MP make_pair
+#define REP(i, a, b) for (int i = a; i < b; i++)
+#define SQ(a) (a) * (a)
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    int t;
+    cin >> t;
+    if (t % 2 == 1)
+        cout << (t / 2 + 1) * (t / 2 + 2)*2;
+    else
+        cout << ((t / 2) + 1) * ((t / 2) + 1);
+}
